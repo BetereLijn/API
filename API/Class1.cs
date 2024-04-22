5ff603d38adb5b9cb7697e2f5bc8d65aba58a572
Daya Huybrechts <daya@huybrechts.one>
2024-04-22 23:17:00 +0200

    second commit

--- API/Class1.cs
+++ API/Class1.cs
@@ -6,11 +6,20 @@ namespace API
     {
         //hiermee kan de httpcall gedaan worden
         private HttpClient client = new HttpClient();
-
+        //hierin slaan we alle gemeentes op
+        public Dictionary<string, int> gemeentes = new Dictionary<string ,int>();
+        ///<summary>
+        ///de start-up dat zorgt dat de key wordt in de header gestoken en de dictionaries worden ingeladen
+        ///</summary>
         public GetAPIData()
         {
             client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "c94acbf3136844b08c1fa4281955241b");
+            refreshGemeentes();
         }
+
+        ///<summary>
+        ///geeft de urls terug in dictionary met location als key en url als value
+        ///</summary>
         public async Task<Dictionary<string,string>> getAPIURLs()
         {
             Dictionary<string, string> d = new Dictionary<string, string>();
@@ -26,6 +35,44 @@ namespace API
                 }
             }
             return d;
+        }
+        ///<summary>
+        ///herlaad de gegevens in Gemeentes
+        ///</summary>
+        private async void refreshGemeentes()
+        {
+            string url = "https://api.delijn.be/DLKernOpenData/api/v1/gemeenten";
+            string response = await client.GetStringAsync(url);
+            using (JsonDocument document = JsonDocument.Parse(response))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement gemeenten = root.GetProperty("gemeenten");
+                foreach(JsonElement gemeente in gemeenten.EnumerateArray())
+                {
+                    try
+                    {
+                        if (gemeente.GetProperty("hoofdGemeente").ToString() != null) {
+                            if (gemeente.GetProperty("omschrijving").ToString() != gemeente.GetProperty("hoofdGemeente").GetProperty("omschrijving").ToString())
+                                gemeentes.Add(gemeente.GetProperty("omschrijving").ToString(), Convert.ToInt32(gemeente.GetProperty("gemeentenummer").ToString()));
+                        }
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        try {
+                            gemeentes.Add(gemeente.GetProperty("omschrijving").ToString(), Convert.ToInt32(gemeente.GetProperty("gemeentenummer").ToString()));
+                        }
+                        catch(Exception)
+                        {
+
+                        }
+                        }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                gemeentes = gemeentes.OrderBy(pair=>pair.Value).ToDictionary();
+            }
         }
     }
 }
